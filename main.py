eff381326ce4eeae23966c88d0b2f720060a8633
GaboGomezT <gomezgabriel09@gmail.com>
2023-10-11 17:40:03 -0600

    baisc form

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, jsonify
 import os
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    if request.method == 'POST':
+        # Handle the form submission
+        nombre = request.form['nombre']
+        actividad = request.form['actividad']
+        cantidad_personas = request.form['cantidad_personas']
+        pago_total = request.form['pago_total']
+        
+        # You can process or save the data as needed.
+        # For now, let's return it as a JSON response:
+        return jsonify({
+            "Nombre": nombre,
+            "Actividad": actividad,
+            "Cantidad de Personas": cantidad_personas,
+            "Pago Total": pago_total
+        })
 
+    return render_template('form.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
